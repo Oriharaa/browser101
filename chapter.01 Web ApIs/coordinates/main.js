a2133e3e9460a9bc668adf24821fadd94aea6495
Oriharara <60921094+Oriharaa@users.noreply.github.com>
2020-12-25 21:00:43 +0900

    Js: Coordinate performance improvement

--- chapter.01 Web ApIs/coordinates/main.js
+++ chapter.01 Web ApIs/coordinates/main.js
@@ -6,13 +6,19 @@ const horozintal = document.querySelector('.horozintal');
 const vertical = document.querySelector('.vertical');
 
 
-document.addEventListener('mousemove', (event)=>{
-  horozintal.style.top = `${event.clientY}px`;
-  vertical.style.left=`${event.clientX}px`;
-  target.style.top = `${event.clientY}px`;
-  target.style.left = `${event.clientX}px`;
-  tag.style.top = `${event.clientY}px`;
-  tag.style.left = `${event.clientX}px`;
-  tag.innerHTML = `${event.clientX}px, ${event.clientY}px`;
-  
+window.addEventListener('load', ()=>{
+  const targetRect = target.getBoundingClientRect();
+  const targetHalfWidth = targetRect.width / 2;
+  const targetHalfHeight = targetRect.height / 2;
+
+  document.addEventListener('mousemove', (event)=>{
+    const posX = event.clientX;
+    const posY = event.clientY;
+    
+    horozintal.style.transform =`translateY(${posY}px)`;
+    vertical.style.transform=`translateX(${posX}px)`;
+    target.style.transform=`translate(${posX-targetHalfWidth}px, ${posY-targetHalfHeight}px)`;
+    tag.style.transform=`translate(${posX+20}px, ${posY+20}px)`;
+    tag.innerHTML=`${posX}px, ${posY}px`;
+  });
 });
